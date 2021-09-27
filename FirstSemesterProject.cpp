a8d94d2800897eeca2c0ffb4591523c43784724e
rddeuser <bdeuser13@gmail.com>
2021-09-27 12:09:17 -0500

    Turn function
    Added cpp file with first version of turn function

---
+++ FirstSemesterProject.cpp
@@ -0,0 +1,83 @@
+/*
+Author: Becca Deuser
+*Date: 09/27/21
+*Email: rddeuser@dmacc.edu
+*Overview: This is the early working model of how a turn would work in the Medieval Game of Life
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//placeholder variables- remove as needed
+int spotOnBoard = 0;
+const int NUM_SPACES = 150;
+string board[NUM_SPACES];
+string typeOfSpaces[NUM_SPACES];
+
+//function declaration
+void turn();
+
+int main() {
+	//continue taking turns while player hasn't reached end of board
+	while (spotOnBoard < NUM_SPACES - 1) {
+		turn();
+	}//end while
+	return 0;
+}//end main
+
+void turn() {
+	//display board
+	//TODO: add display function
+
+	//display money, cards in hand, etc.
+	// TODO: add card display function
+
+
+	//declare and initialize spin
+	int spin = rand() % 10 + 1;
+
+	//display spin to user
+	cout << "Spinning spinner..." << endl;
+	cout << "You have spun a " << spin << endl;
+
+	//check for long term investments
+	//TODO: add long-term investment
+
+	//move player number of spaces if possible
+	spotOnBoard += spin;
+
+	if (spotOnBoard >= NUM_SPACES) {
+		spotOnBoard = NUM_SPACES - 1; //if spot would exceed index, move to last index
+	}//end if
+
+	//change spot on board to represent player
+	board[spotOnBoard] = "O"; //or however we're representing their spot on the board
+
+	//display board
+	// TODO: add display function
+
+	//if statement to determine what it is they landed on
+	if (typeOfSpaces[spotOnBoard] == "") {//TODO: add types of spaces
+		//TODO: add function
+	}//end if
+	else if (typeOfSpaces[spotOnBoard] == "") {//TODO: add types of spaces
+		//TODO: add function
+	}//end else if
+	else if (typeOfSpaces[spotOnBoard] == "") {//TODO: add types of spaces
+		//TODO: add function
+	}//end else if
+	else if (typeOfSpaces[spotOnBoard] == "") {//TODO: add types of spaces
+		//TODO: add function
+	}//end else if
+	else {
+		//TODO: add function
+	}//end else
+	//TODO: Finish if statements
+}//end turn
+/*------TESTED--------
+*
+*
+*
+*
+*/
